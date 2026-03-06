2db6ab0ff1501f3200af6f065e881783b23b5472
Vivek <vivek.mitra@outlook.com>
2026-03-07 09:43:17 +1100

    feature: savingsRelief

--- src/domain/engine/calculateSavingsRelief.ts
+++ src/domain/engine/calculateSavingsRelief.ts
@@ -1,8 +1,45 @@
+import { time } from "console";
 import { TimelineEvent } from "../types/forecast";
+import { SavingsRelief } from "../types/forecast";
 //iterate through the timeline
 //find the point where the running balance becomes negative or is minimum
 //check if it is greater than or equal to buffer, if yes, return null
-//if no, then gather all events that led up to this minevent and have the type commitment
+//if no, then gather all events that led up to this minevent and have the type = commitment
 //pick the most largest commitment
 //reliefAMount = abs(minEvent.runningBalance) + buffer
 //if reliefAMount > targetCommitment.amount, reliefAMount = targetCommitment.amount
+
+export function calculateSavingsRelief(
+  timelineEvents: TimelineEvent[],
+  buffer: number,
+): SavingsRelief | {} {
+  const minEvent: TimelineEvent = timelineEvents.reduce((min, e) =>
+    e.runningBalance < min.runningBalance ? e : min,
+  );
+  if (minEvent.runningBalance >= buffer) {
+    return {};
+  }
+  const reliefGap: number = Math.abs(minEvent.runningBalance) + buffer;
+
+  const allCommitmentEventsTillMinEvent: TimelineEvent[] =
+    timelineEvents.filter(
+      (i) => i.type === "commitment" && i.timestamp <= minEvent.timestamp,
+    );
+
+  const highestCommitment: TimelineEvent =
+    allCommitmentEventsTillMinEvent.reduce((high, e) =>
+      high.amount > e.amount ? high : e,
+    );
+
+  /*
+    export const SavingsReliefSchema = z.object({
+      targetEventId: z.string(),
+      targetLabel: z.string(),
+      reliefAmount: z.number(),
+      remainingCommitment: z.number(),
+      predictedBalance: z.number(),
+      isFullyResolved: z.boolean(),
+    });
+     */
+  return {};
+}
